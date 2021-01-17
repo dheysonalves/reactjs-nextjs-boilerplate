c55480aa9dcb165d7e8f5c7d2b60d3f13f76fe77
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-01-17 15:17:39 -0300

    feat: Adding pwa config for the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
 				<title>React Avancado - Boilerplate</title>
 				<link rel="shortcut icon" href="/img/icon-512.png" />
 				<link rel="apple-touch-icon" href="/img/icon-512.png" />
+				<link rel="manifest" href="/manifest.json" />
 				<meta
 					name="description"
 					content="A simple project starter to work with typescript, React, NextJs and Styled Components"
